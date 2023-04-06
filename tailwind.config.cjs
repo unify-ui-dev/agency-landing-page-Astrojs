441ce5c3003864ab7122cf984465f1c3ccc68521
Johnk-Mj <johnkatembue4@gmail.com>
2023-04-06 23:11:51 +0200

    update tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: "class",
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				body: "rgb(var(--color-bg))",
+				"box-bg": "rgb(var(--color-box))",
+				"box-shadow": "rgb(var(--box-sd))",
+				"box-border": "rgb(var(--box-border))",
+				primary: "#1d4ed8",
+				"heading-1": "rgb(var(--heading-1))",
+				"heading-2": "rgb(var(--heading-2))",
+				"heading-3": "rgb(var(--heading-3))",
+			},
+			screens:{
+				midmd:"880px"
+			}
+		},
 	},
 	plugins: [],
 }
