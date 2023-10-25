de2f2476032b42484a4ee1f5952977e6fe19d886
Alex Pashkin <paas.pashkin@gmail.com>
2023-10-25 23:28:25 +0500

    upd.vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/static';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: 'server',
+  adapter: vercel(),
 });
