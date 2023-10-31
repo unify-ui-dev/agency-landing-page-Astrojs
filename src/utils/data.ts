55ddf8c33b82bee1d1a96913291130a6cf935fa3
Mamadou Mb. Dione <47165947+mbayedione10@users.noreply.github.com>
2023-10-31 15:57:37 +0000

    ♻️ refactor (Services): Refactor service rendering logic using data file

---
+++ src/utils/data.ts
@@ -0,0 +1,29 @@
+const services = [
+    {
+        title: "Social media marketing",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate"
+    },
+    {
+        title: "Amazon affiliate marketing",
+        description: "Sunt, ipsam, necessitatibus sint fugit officia laboriosam minima ab ullam at magni et. Quaerat, sint!"
+    },
+    {
+        title: "Email marketing",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate"
+    },
+    {
+        title: "Social media marketing",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate"
+    },
+    {
+        title: "Amazon affiliate marketing",
+        description: "Sunt, ipsam, necessitatibus sint fugit officia laboriosam minima ab ullam at magni et. Quaerat, sint!"
+    },
+    {
+        title: "Email marketing",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate"
+    },
+
+]
+
+export { services }
