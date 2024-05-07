dcc8865450bcd31036cdd30984d0d352434c53e4
André Silva <andre.silva@posteo.de>
2024-05-07 12:23:15 +1000

    Update URL after domain https isssue

--- src/utils/work.ts
+++ src/utils/work.ts
@@ -1,30 +1,26 @@
 const projects = [
-    {
-      title: "Aldeia Djembe Camp",
-      description:
-        "A landing page for a dance and drum retreat in Portugal.",
-      image: `/images/screenshot-adc.png`,
-      position: "left",
-      url: "https://aldeia-djembe-camp.com/",
-    },
-    {
-      title: "Wayfaring",
-      description:
-        "Recruitment agency based in Melbourne",
-      image: `/images/screenshot-wayfaring.png`,
-      position: "left",
-      url: "https://mintcream-mule-391347.builder-preview.com",
+  {
+    title: "Aldeia Djembe Camp",
+    description: "A landing page for a dance and drum retreat in Portugal.",
+    image: `/images/screenshot-adc.png`,
+    position: "left",
+    url: "https://aldeia-djembe-camp.com/",
+  },
+  {
+    title: "Wayfaring",
+    description: "Recruitment agency based in Melbourne",
+    image: `/images/screenshot-wayfaring.png`,
+    position: "left",
+    url: "https://wayfaring.hinterlandweb-folio.in/",
+  },
+  {
+    title: "Sala",
+    description: "A invoicing solution for online tutors",
+    image: `/images/screenshot-sala.png`,
+    url: "https://final-project-sala.vercel.app/",
+  },
 
-    },
-    {
-      title: "Sala",
-      description:
-        "A invoicing solution for online tutors",
-      image: `/images/screenshot-sala.png`,
-      url: "https://final-project-sala.vercel.app/",
-    },
-    
-    ,
-  ];
-  
-  export { projects };
+  ,
+];
+
+export { projects };
