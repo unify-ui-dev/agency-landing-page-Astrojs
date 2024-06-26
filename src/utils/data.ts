85b138c3e8d8775ecc348c8d9dea0135f16a5e07
André Silva <andre.silva@posteo.de>
2024-06-26 12:51:13 +1000

    Update Services and Footer

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -32,9 +32,9 @@ const services = [
   </svg>`,
   },
   {
-    title: "Social Media",
+    title: "SEO",
     description:
-      "Although not a central part of our services, we can help you kickstart or improve your social media presence",
+      "Be found on Google! We help you outline a strategy so that you rank higher in search results with your brand new website",
     icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="w-6 h-6">
     <path fill="black" d="M3 20.077V4.616q0-.691.463-1.153T4.615 3H14.6q-.061.25-.071.49t-.006.51H4.616q-.231 0-.424.192T4 4.615v13.03L5.65 16h13.735q.23 0 .423-.192t.192-.423V8.342q.287-.067.527-.155q.24-.087.473-.225v7.423q0 .69-.462 1.153T19.385 17H6.077zM4 4.616v12.769V4zm15 1.846q-1.038 0-1.77-.731t-.73-1.77t.73-1.769t1.77-.73t1.77.73t.73 1.77t-.73 1.769t-1.77.73" />
   </svg>`,
