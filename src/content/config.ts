2daa5a9fffd40384d938e8271b3a919c75afdc54
André Silva <andre.silva@posteo.de>
2024-06-28 09:46:36 +1000

    Connect with R2

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,5 @@
 // 1. Import utilities from `astro:content`
-import { z, defineCollection } from 'astro:content';
+import { z, defineCollection } from "astro:content";
 
 // 2. Define your collection(s)
 const blogCollection = defineCollection({
---
+++ src/content/team/andre-silva.md
@@ -0,0 +1,10 @@
+---
+draft: false
+name: "André Silva"
+title: "Founder & Lifelong Learner"
+avatar: {
+    src: "https://www.hinterlandweb.com/LI%20profile%20pic.jpeg",
+    alt: "André Silva"
+}
+publishDate: "2022-11-07 15:39"
+---
